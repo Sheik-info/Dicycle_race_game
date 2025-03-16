b51539c454a8dc320d486c9abc4a113860389c47
Sheik-info <cheikh_ba1@outlook.fr>
2025-03-16 18:30:00 +0100

    Update jouerPartie_vue.php

--- vues/jouerPartie_vue.php
+++ vues/jouerPartie_vue.php
@@ -1,315 +1,135 @@
 <form action="index.php?page=jouerPartie" method="post">
-
     <?php
-    //les nombre de joueurs vont être initialisé et la stratégie
-    if (isset($_POST['nbjoueur']) == false && isset($_POST['partie']) == false && isset($_POST['commencez']) == false && isset($_POST['lancer']) == false) {
-        echo "    <div class=\"jouer\">
-	
-	     <label for=\"nbjoueur\">Nombre de joueurs:</label>
-        <select name=\"nbjoue\">
-            <option value=1>1</option>
-            <option value=2>2</option>
-            <option value=3>3</option>
-            <option value=4>4</option>
-            <option value=5>5</option>
-        </select>
-        <br><br>
-       <label for=\"strat\">Pour l'ordre de passage:</label>
-        <select name=\"contrainte\">
-            <option value=\"ALEA\">Aléatoire</option>
-            <option value=\"+JEUNE\">Le plus jeune</option>
-            <option value=\"-EXPE\">Le moins d'experience</option>
-        </select>
-        <br><br>
-        
-        <input type=\"submit\" value=\"envoyer\" name=\"nbjoueur\" style=\"width: 60px; height: 30px\"/>";
-        //le formulaire pour les données des joueurs vont être mis dans la base
-    } else if (isset($_POST['nbjoueur'])) {
+    // Fonction pour générer un menu déroulant
+    function generateSelect($name, $label, $options, $id = '') {
+        echo "<label for=\"$name\">$label :</label>";
+        echo "<select name=\"$name\"" . ($id ? " id=\"$id\"" : "") . ">";
+        foreach ($options as $value => $text) {
+            echo "<option value=\"$value\">$text</option>";
+        }
+        echo "</select><br><br>";
+    }
 
+    // Initialisation du nombre de joueurs et de la stratégie
+    if (!isset($_POST['nbjoueur']) && !isset($_POST['partie']) && !isset($_POST['commencez']) && !isset($_POST['lancer']) && !isset($_POST['tourSuivant'])) {
+        echo '<div class="jouer">';
+        $nbJoueursOptions = [1 => 1, 2 => 2, 3 => 3, 4 => 4, 5 => 5];
+        generateSelect("nbjoue", "Nombre de joueurs", $nbJoueursOptions);
 
-        // géneration des formulaires des joueus pour le mettre dans la base
+        $strategieOptions = [
+            "ALEA" => "Aléatoire",
+            "+JEUNE" => "Le plus jeune",
+            "-EXPE" => "Le moins d'expérience"
+        ];
+        generateSelect("contrainte", "Pour l'ordre de passage", $strategieOptions);
+
+        echo '<input type="submit" value="Envoyer" name="nbjoueur" style="width: 60px; height: 30px" />';
+        echo '</div>';
+    }
+    // Formulaire pour les données des joueurs
+    else if (isset($_POST['nbjoueur'])) {
+        echo '<div class="jouer">';
         for ($i = 1; $i <= $_POST['nbjoue']; $i++) {
-            echo  "<h2>Données des Joueurs $i</h2>";
-
-            echo "<label for=\"nom$i\">Saisissez votre nom:</label>
-        <input type=\"text\" id=\"nom$i\" name=\"nom$i\" required minlength=\"2\" maxlength=\"11\" size=\"20\" /><br><br>
-        
-        <label for=\"prenom$i\">Saisissez votre prenom:</label>
-        <input type=\"text\" id=\"prenom$i\" name=\"prenom$i\" required minlength=\"2\" maxlength=\"11\" size=\"20\" /><br><br>
-        
-        <label for=\"pseudo$i\">Saisissez votre pseudo:</label>
-        <input type=\"text\" id=\"pseudo$i\" name=\"pseudo$i\" required minlength=\"4\" maxlength=\"11\" size=\"20\" /><br><br>
-        
-        <label for=\"mail$i\">Saisissez votre email:</label>
-        <input type=\"text\" id=\"mail$i\" name=\"mail$i\" required minlength=\"4\" maxlength=\"20\" size=\"20\" /><br><br>
-        
-        <label for=\"naiss$i\">Saisissez votre date de naissance:</label>
-        <input type=\"text\" id=\"naiss$i\" name=\"naiss$i\" required minlength=\"4\" maxlength=\"20\" size=\"20\" /><br><br><br><br>";
+            echo "<h2>Données du Joueur $i</h2>";
+            echo "<label for=\"nom$i\">Saisissez votre nom :</label>";
+            echo "<input type=\"text\" id=\"nom$i\" name=\"nom$i\" required minlength=\"2\" maxlength=\"11\" size=\"20\" /><br><br>";
+            echo "<label for=\"prenom$i\">Saisissez votre prénom :</label>";
+            echo "<input type=\"text\" id=\"prenom$i\" name=\"prenom$i\" required minlength=\"2\" maxlength=\"11\" size=\"20\" /><br><br>";
+            echo "<label for=\"pseudo$i\">Saisissez votre pseudo :</label>";
+            echo "<input type=\"text\" id=\"pseudo$i\" name=\"pseudo$i\" required minlength=\"4\" maxlength=\"11\" size=\"20\" /><br><br>";
+            echo "<label for=\"mail$i\">Saisissez votre email :</label>";
+            echo "<input type=\"email\" id=\"mail$i\" name=\"mail$i\" required minlength=\"4\" maxlength=\"20\" size=\"20\" /><br><br>";
+            echo "<label for=\"naiss$i\">Saisissez votre date de naissance :</label>";
+            echo "<input type=\"date\" id=\"naiss$i\" name=\"naiss$i\" required /><br><br><br>";
         }
-        echo "<input type=\"submit\" name=\"partie\" value=\"Commencer\" style=\"width: 90px; height: 30px\"/><br><br><br><br>";
+        echo '<input type="submit" name="partie" value="Commencer" style="width: 90px; height: 30px" /><br><br>';
 
-        echo "<p>Pions</p>";
-        //mélange de pions aléatoire
+        // Affichage des pions
+        echo "<p>Pions :</p>";
         shuffle($pions);
-        for ($i = 0; $i < $_POST['nbjoue']; ++$i) {
-
-            echo "
-        <a>
-		<img src=\"./ressources_graphiques/$pions[$i]\" />
-		</a>";
+        for ($i = 0; $i < $_POST['nbjoue']; $i++) {
+            echo "<a><img src=\"./ressources_graphiques/{$pions[$i]}\" alt=\"Pion $i\" /></a>";
         }
-        //je montre les cartes
-        echo "<p>Les cartes du plateau:</p><br>
-                <a>
-		<img src=\"./ressources_graphiques/carteDépart.png\">
-		</a>
-		
-        ";
-        for ($i = 1; $i <= 75; ++$i) {
-            echo   " <a>
-		<img src=\"./ressources_graphiques/C0$i.png\" />
-		</a>";
+
+        // Affichage des cartes du plateau
+        echo "<p>Les cartes du plateau :</p><br>";
+        echo '<a><img src="./ressources_graphiques/carteDépart.png" alt="Carte de départ"></a>';
+        for ($i = 1; $i <= 75; $i++) {
+            $cardNumber = str_pad($i, 2, "0", STR_PAD_LEFT); // Format C01, C02, etc.
+            echo "<a><img src=\"./ressources_graphiques/C$cardNumber.png\" alt=\"Carte $i\" /></a>";
         }
-        echo "
-		<a>
-		<img src=\"./ressources_graphiques/carteArrivée.png\"/>
-		</a>
-        <br><br>";
-
-        echo " </div>";
+        echo '<a><img src="./ressources_graphiques/carteArrivée.png" alt="Carte d\'arrivée" /></a><br><br>';
+        echo '</div>';
     }
-    //formulaire pour mettre en place le jeu
-    if (isset($_POST['partie'])) {
-        //formulaire pour la selection des cartes
-        echo "    <div class=\"jouer\">
+    // Formulaire pour sélectionner les cartes
+    else if (isset($_POST['partie'])) {
+        echo '<div class="jouer">';
+        echo '<h2>Jouer une partie</h2>';
+        echo '<p>12 cartes maximum</p>';
 
-        <h2>Jouer une partie</h2>
-        <p>12 cartes maximum </p>
-        
-        <label for=\"nbverte\">Nombre de cartes vertes à sélectionner:</label>
-        <select name=\"nbcv\" id=\"nombre de cartes\">
-            <option value=0>0</option>
-            <option value=1>1</option>
-            <option value=2>2</option>
-            <option value=3>3</option>
-            <option value=4>4</option>
-            <option value=5>5</option>
-            <option value=6>6</option>
-            <option value=7>7</option>
-            <option value=8>8</option>
-            <option value=9>9</option>
-            <option value=10>10</option>
-            <option value=11>11</option>
-            <option value=12>12</option>
-        </select>
-        <br><br>
-        
-        <label for=\"nbjaune\">Nombre de cartes jaunes à sélectionner:</label>
-        <select name=\"nbcj\" id=\"nombre de cartes\">
-            <option value=0>0</option>
-            <option value=1>1</option>
-            <option value=2>2</option>
-            <option value=3>3</option>
-            <option value=4>4</option>
-            <option value=5>5</option>
-            <option value=6>6</option>
-            <option value=7>7</option>
-            <option value=8>8</option>
-            <option value=9>9</option>
-            <option value=10>10</option>
-            <option value=11>11</option>
-            <option value=12>12</option>
-        </select>
-        <br><br>
-        
-        <label for=\"nbbleu\">Nombre de cartes bleus à sélectionner:</label>
-        <select name=\"nbcb\" id=\"nombre de cartes\">
-            <option value=0>0</option>
-            <option value=1>1</option>
-            <option value=2>2</option>
-            <option value=3>3</option>
-            <option value=4>4</option>
-            <option value=5>5</option>
-            <option value=6>6</option>
-            <option value=7>7</option>
-            <option value=8>8</option>
-            <option value=9>9</option>
-            <option value=10>10</option>
-            <option value=11>11</option>
-            <option value=12>12</option>
-        </select>
-        <br><br>
+        $cardOptions = range(0, 12);
+        generateSelect("nbcv", "Nombre de cartes vertes à sélectionner", array_combine($cardOptions, $cardOptions), "nombre de cartes");
+        generateSelect("nbcj", "Nombre de cartes jaunes à sélectionner", array_combine($cardOptions, $cardOptions), "nombre de cartes");
+        generateSelect("nbcb", "Nombre de cartes bleues à sélectionner", array_combine($cardOptions, $cardOptions), "nombre de cartes");
+        generateSelect("nbcn", "Nombre de cartes noires à sélectionner", array_combine($cardOptions, $cardOptions), "nombre de cartes");
 
-        <label for=\"nbnoir\">Nombre de cartes noires à sélectionner:</label>
-        <select name=\"nbcn\" id=\"nombre de cartes\">
-            <option value=0>0</option>
-            <option value=1>1</option>
-            <option value=2>2</option>
-            <option value=3>3</option>
-            <option value=4>4</option>
-            <option value=5>5</option>
-            <option value=6>6</option>
-            <option value=7>7</option>
-            <option value=8>8</option>
-            <option value=9>9</option>
-            <option value=10>10</option>
-            <option value=11>11</option>
-            <option value=12>12</option>
-        </select>
-        <br><br>
-        
-        
-        <input type=\"submit\" name=\"commencez\" value=\"envoyer\" style=\"width: 60px; height: 60px\"/>
-        
-        
-        </div>";
+        echo '<input type="submit" name="commencez" value="Envoyer" style="width: 60px; height: 60px" />';
+        echo '</div>';
     }
-
     ?>
+
     <div class="joueur">
-
         <?php
-
-        $tour = 0;
-        //commencement de la partie, gestion du tour
+        // Gestion des tours
         if (isset($_POST['commencez']) || isset($_POST['tourSuivant']) || isset($_POST['lancer'])) {
+            static $tour = 0; // Utilisation de static pour conserver la valeur entre les appels
             $tour += 1;
-
-
-            //$pions $i jusqu'à $_POST['nbj']
             echo "Il s'agit du tour $tour<br><br>";
-            // echo getCarte("bleu"); testé il marche bien
 
-            //selections des dés et lancement des dés
-            echo "<label for=\"nbde\">Choisissez 6 dés au total:</label><br><br>
-	<label for=\"nbrouge\">Nombre de dés rouges:</label>
-        <select name=\"nbdr\" id=\"nombre de dés rouges\">
-            <option value=0>0</option>
-            <option value=1>1</option>
-            <option value=2>2</option>
-            <option value=3>3</option>
-            <option value=4>4</option>
-            <option value=5>5</option>
-            <option value=6>6</option>
-        </select>
-        <br><br>
-        <label for=\"nbbleus\">Nombre de dés bleus:</label>
-                <select name=\"nbdb\" id=\"nombre de dés bleus\">
-            <option value=0>0</option>
-            <option value=1>1</option>
-            <option value=2>2</option>
-            <option value=3>3</option>
-            <option value=4>4</option>
-            <option value=5>5</option>
-            <option value=6>6</option>
-        </select>
-        <br><br>
-        <label for=\"nbjaune\">Nombre de dés oranges:</label>
-            <select name=\"nbdo\" id=\"nombre de dés oranges\">
-            <option value=0>0</option>
-            <option value=1>1</option>
-            <option value=2>2</option>
-            <option value=3>3</option>
-            <option value=4>4</option>
-            <option value=5>5</option>
-            <option value=6>6</option>
-        </select>
-        <br><br>
-        
-        <input type=\"submit\" name=\"lancer\" value=\"lancer\" style=\"width: 60px; height: 30px\"/><br><br>";
+            // Sélection des dés
+            $diceOptions = range(0, 6);
+            echo '<label for="nbde">Choisissez 6 dés au total :</label><br><br>';
+            generateSelect("nbdr", "Nombre de dés rouges", array_combine($diceOptions, $diceOptions), "nombre de dés rouges");
+            generateSelect("nbdb", "Nombre de dés bleus", array_combine($diceOptions, $diceOptions), "nombre de dés bleus");
+            generateSelect("nbdo", "Nombre de dés oranges", array_combine($diceOptions, $diceOptions), "nombre de dés oranges");
+            echo '<input type="submit" name="lancer" value="Lancer" style="width: 60px; height: 30px" /><br><br>';
 
+            // Lancer des dés et affectation
             if (isset($_POST['lancer'])) {
-                for ($i = 0; $i < $_POST['nbdr']; ++$i) {
-                    $des[] = lancer_de("rouge");
-                }
-                for ($i = 0; $i < $_POST['nbdb']; ++$i) {
-                    $des[] = lancer_de("bleu");
-                }
-                for ($i = 0; $i < $_POST['nbdo']; ++$i) {
-                    $des[] = lancer_de("jaune");
-                }
-                for ($i = 0; $i < 6; ++$i) {
-                    echo    "<a>
-		        <img src=\"./ressources_graphiques/éléments_graphiques/$des[$i]\" alt=\"dé\" style=\"width: 80px; height: 80px\"/>
-		        </a>
-                <label for=\"nbjaune\">Affecter à une carte</label>
-                <select name=\"affectation\" id=\"nombre de dés oranges\">
-                <option value=1>carte 1</option>
-                <option value=2>carte 2</option>
-                <option value=3>carte 3</option>
-                <option value=4>carte 4</option>
-                <option value=5>carte 5</option>
-                <option value=6>carte 6</option>
-                <option value=7>carte 7</option>
-                <option value=7>carte 8</option>
-                <option value=7>carte 9</option>
-                <option value=7>carte 10</option>
-                <option value=7>carte 11</option>
-                <option value=7>carte 12</option>
-                </select><br><br>
-                ";
+                $des = [];
+                for ($i = 0; $i < $_POST['nbdr']; $i++) $des[] = lancer_de("rouge");
+                for ($i = 0; $i < $_POST['nbdb']; $i++) $des[] = lancer_de("bleu");
+                for ($i = 0; $i < $_POST['nbdo']; $i++) $des[] = lancer_de("jaune");
+
+                $cardOptions = range(1, 12);
+                foreach ($des as $index => $de) {
+                    echo "<a><img src=\"./ressources_graphiques/éléments_graphiques/$de\" alt=\"Dé $index\" style=\"width: 80px; height: 80px\" /></a>";
+                    echo "<label for=\"affectation$index\">Affecter à une carte :</label>";
+                    echo "<select name=\"affectation[$index]\" id=\"affectation$index\">";
+                    foreach ($cardOptions as $value) {
+                        echo "<option value=\"$value\">Carte $value</option>";
+                    }
+                    echo "</select><br><br>";
                 }
             }
-            echo "<br><br>";
 
-
-            //je mets pas ce genre de variables dans le controleur car le site web ne le détecte pas
-            $temp;
-            echo "<a>
-		<img src=\"./ressources_graphiques/carteDépart.png\" alt=\"départ\" />
-		</a>";
-            //j'aurais mis $nombre de joueurs mais vu que les requetes ne marchent pas
-            for ($i = 0; $i < 1; ++$i) {
-                echo "<a>
-		<img src=\"./ressources_graphiques/$pions[$i]\" alt=\"imagine\" class=\"absolute\"/>
-		</a>";
+            // Affichage du plateau de jeu
+            echo '<a><img src="./ressources_graphiques/carteDépart.png" alt="Départ" /></a>';
+            for ($i = 0; $i < 1; $i++) {
+                echo "<a><img src=\"./ressources_graphiques/{$pions[$i]}\" alt=\"Pion $i\" class=\"absolute\" /></a>";
             }
 
-            //affichage du plateau de jeu
-            for ($i = 1; $i < $_POST['nbcb']; ++$i) {
-
-                $temp = getCarte("bleu");
-                $cartes[] = "$temp";
-                $temp = "./ressources_graphiques/" . '$temp';
-                echo "<a>
-		<img src='$temp' alt=\"cartebleu\" class=\"relative\"/>
-		</a>";
+            $cartes = [];
+            foreach (["bleu" => "nbcb", "vert" => "nbcv", "jaune" => "nbcj", "noir" => "nbcn"] as $color => $nbKey) {
+                for ($i = 1; $i < $_POST[$nbKey]; $i++) {
+                    $temp = getCarte($color);
+                    $cartes[] = $temp;
+                    echo "<a><img src=\"./ressources_graphiques/$temp\" alt=\"Carte $color\" class=\"relative\" /></a>";
+                }
             }
-            for ($i = 1; $i < $_POST['nbcv']; ++$i) {
-
-                $temp = getCarte("vert");
-                $cartes[] = "$temp";
-                $temp = "./ressources_graphiques/" . '$temp';
-                echo "<a>
-		<img src='$temp' alt=\"carteverte\" class=\"relative\"/>
-		</a>";
-            }
-            for ($i = 1; $i < $_POST['nbcj']; ++$i) {
-                $temp = getCarte("orange");
-                $cartes[] = "$temp";
-                $temp = "./ressources_graphiques/" . '$temp';
-                echo "<a>
-		<img src='$temp' alt=\"carteorange\" class=\"relative\"/>
-		</a>";
-            }
-            for ($i = 1; $i < $_POST['nbcn']; ++$i) {
-                $temp = getCarte("noir");
-                $cartes[] = "$temp";
-                $temp = "./ressources_graphiques/" . '$temp';
-                echo "<a>
-		<img src='$temp' alt=\"cartenoir\" class=\"relative\"/>
-		</a>";
-            }
-            echo "<a>
-		<img src=\"./ressources_graphiques/carteArrivée\" alt=\"fin\" class=\"relative\"/>
-		</a>";
-
-
-            echo "<br><br>";
+            echo '<a><img src="./ressources_graphiques/carteArrivée.png" alt="Arrivée" class="relative" /></a><br><br>';
+            echo '<input type="submit" name="tourSuivant" value="Tour suivant" style="width: 90px; height: 30px" />';
         }
-
         ?>
-
     </div>
-
 </form>
